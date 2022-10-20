e671bc2d09da9cf479a46628cab8ab676ec4b15c
Zahar <zahazet2@gmail.com>
2022-10-20 16:53:28 +0300

    add personalMovieDB

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+
+const personalMovieDB = {
+  count: personalMovieDB,
+  movies: {},
+  actors:  {},
+  genres: [],
+  privat: false
+};
