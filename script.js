264614b28717bd4a79081470f83d0a89a15b71fb
Zahar <zahazet2@gmail.com>
2022-10-20 17:05:46 +0300

    personalMovieDB add b and d

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const personalMovieDB = {
 };
 
 const a = prompt('Один з останніх фільмів які ви подивились?', ''),
-      b = prompt('Оцініть цей фільм', ''),
-      c = prompt('Один з останніх фільмів які ви подивились?', ''),
-      d = prompt('Оцініть цей фільм', '');
+  b = prompt('Оцініть цей фільм', ''),
+  c = prompt('Один з останніх фільмів які ви подивились?', ''),
+  d = prompt('Оцініть цей фільм', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
